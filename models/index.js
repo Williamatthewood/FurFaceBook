c2b8cad17b8ee78bb2e2646fb9ff91e6b58b2160
Williamatthewood <w.wood@tcu.edu>
2023-05-30 19:27:13 -0500

    added index.js for models

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const Post = require('./Post');
+const Event = require('./Event');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Event, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Event.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Post };
