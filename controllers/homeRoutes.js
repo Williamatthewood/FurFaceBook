8826ec12dc46f604dd51e3f44fddeec5e269fc34
Cesar Garcia <CGarcia@heads-up.com>
2023-06-06 12:18:53 -0500

    Add code to handle images in Heroku and localhost

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -42,7 +42,8 @@ router.get('/post/:id', async (req, res) => {
 
     res.render('post', {
       ...post,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
+      is_heroku: req.session.is_heroku
     });
   } catch (err) {
     res.status(500).json(err);
